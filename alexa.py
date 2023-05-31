bac854599a742a242b99b95786ed9f5e103dd299
Kartik Chikenakoppa <125461790+ftkartik@users.noreply.github.com>
2023-05-31 22:22:05 +0530

    Add files via upload

---
+++ alexa.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+listener =sr.Recognizer()
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+def take_command():
+    try:
+        
+        with sr.Microphone() as source:
+            print('listening')
+            voice=listener.listen(source)
+            command=listener.recognize_google(voice)
+            command=command.lower()
+            if 'google' in command:
+                command=command.replace('google','')
+                print(command)
+    except:
+        pass
+    return command
+def run_google():
+    command=take_command()
+    if 'play' in command:
+        song=command.replace('play','')
+        talk('playing'+song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time=datetime.datetime.now().strftime('%H:%M')
+        talk('Current time is,' +time)
+
+run_google()
+        
